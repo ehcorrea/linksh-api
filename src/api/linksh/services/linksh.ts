766608ad7f5d0d4680b7bfad0991a6070cdfd21b
Emilio Correa <ehcorrea60@gmail.com>
2023-12-02 23:36:47 -0300

    feat: intercepta e adiciona valores ao metodo create

--- src/api/linksh/services/linksh.ts
+++ src/api/linksh/services/linksh.ts
@@ -2,6 +2,42 @@
  * linksh service
  */
 
+import { errors } from '@strapi/utils';
 import { factories } from '@strapi/strapi';
+import { v4 } from 'uuid';
 
-export default factories.createCoreService('api::linksh.linksh');
+import { LinkshCreateRequest } from '../../../../types/linksh';
+import { User } from '../../../../types/user';
+
+const { ApplicationError } = errors;
+
+export default factories.createCoreService('api::linksh.linksh', () => ({
+
+  async create(args) {
+
+    const { data: { title, content, timeout }, }: LinkshCreateRequest = args
+
+    const actualDateISOString = new Date().toISOString();
+    const timeoutDateISOString = new Date(timeout).toISOString();
+
+    if (actualDateISOString >= timeoutDateISOString) {
+      throw new ApplicationError('The timeout date must be greater than the current date', { field: 'timeout' });
+    }
+
+    const { id: userId, username } = strapi.requestContext.get().state.user as User;
+
+    const linkshId = `${username}-${v4()}`;
+    const newLinksh = {
+      title,
+      content,
+      timeout: new Date(timeout),
+      linkshId,
+      owner: userId,
+      ownedBy: username
+    };
+
+
+    return await super.create({ data: newLinksh })
+
+  }
+}));
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -391,12 +391,17 @@ export interface ApiLinkshLinksh extends Schema.CollectionType {
     isDeleted: Attribute.Boolean &
       Attribute.Required &
       Attribute.DefaultTo<false>;
-    linkshId: Attribute.UID & Attribute.Required;
-    owned_by: Attribute.Relation<
+    linkshId: Attribute.UID<'api::linksh.linksh', 'ownedBy'> &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 10;
+      }>;
+    owner: Attribute.Relation<
       'api::linksh.linksh',
       'manyToOne',
       'plugin::users-permissions.user'
     >;
+    ownedBy: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -528,6 +533,50 @@ export interface PluginUploadFolder extends Schema.CollectionType {
       Attribute.Private;
     updatedBy: Attribute.Relation<
       'plugin::upload.folder',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
+  info: {
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.SetMinMax<{
+        min: 1;
+        max: 50;
+      }>;
+    code: Attribute.String & Attribute.Unique;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'plugin::i18n.locale',
       'oneToOne',
       'admin::user'
     > &
@@ -691,50 +740,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
-  info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
-    description: '';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<{
-        min: 1;
-        max: 50;
-      }>;
-    code: Attribute.String & Attribute.Unique;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -748,10 +753,10 @@ declare module '@strapi/types' {
       'api::linksh.linksh': ApiLinkshLinksh;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
+      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'plugin::i18n.locale': PluginI18NLocale;
     }
   }
 }
